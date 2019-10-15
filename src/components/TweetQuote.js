a15ca93fa3b8a243e98b9c8c011b7153657fe62f
Steve Golden <sgolden2525@outlook.com>
2019-10-15 11:00:50 -0500

    refactor components
    Added components folder
    
    Added file for each component
    
    Added files to components folder

---
+++ src/components/TweetQuote.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+const TweetQuote = (props) => {
+  return (
+    <a href="https://www.twitter.com/intent/tweet" rel="noopener noreferrer" target="_blank" id="tweet-quote" className="twitter fab fa-twitter-square fa-2x"></a>
+  );
+}
+
+export default TweetQuote;
